731017f24de70247b7730c68e55faf959932198f
ygdokim <ygdokim@gmail.com>
2026-05-31 20:50:22 +0900

    fix all

--- main.js
+++ main.js
@@ -1,32 +1,23 @@
- 'use strict';
-        //체크박스
-        const taskItems = document.querySelectorAll('li');
-        for (let i = 0; i<taskItems.length; i++){
-            const checkBox = taskItems[i].querySelector('input');
-            checkBox.addEventListener('change', function() {
-                if (checkBox.checked) {
-                    taskItems[i].style.color = 'lightgrey';
-                    taskItems[i].style.textDecoration = 'line-through';
-                }else {
-                    taskItems[i].style.color = '';
-                    taskItems[i].style.textDecoration = '';
-                }
-            });
-        };
+"use strict";
+const TaskList = document.getElementById("TaskList");
+const task = document.getElementById("textbox");
 
-        //새로운 할 일 추가
-        let textBox = document.querySelector('#textbox');
-        const addButton = document.querySelector('#add');
-        const taskList = document.querySelector('#TaskList');
-        
-        function addList(_task) {
-            let newItem = document.createElement('li');
-            const newCheckBox = document.createElement('input');
-            taskList.appendChild(newItem);
-            newItem.textContent = _task;
-            newCheckBox.setAttribute('type', 'checkbox');
-            newItem.appendChild(newCheckBox);
-        };
-        addButton.onclick = function addTask() {
-            addList(textBox.value);
-        };
+function addTodo() {
+  const newList = document.createElement("li");
+  TaskList.appendChild(newList);
+  const newCheckBox = document.createElement("input");
+  newCheckBox.setAttribute("type", "checkbox");
+  newList.appendChild(newCheckBox);
+  newCheckBox.addEventListener("change", function (event) {
+    if (event.target.checked === true) {
+      event.target.nextSibling.className = "checked";
+    } else {
+      event.target.nextSibling.className = "";
+    }
+  });
+  const span = document.createElement("span");
+  span.innerText = task.value;
+  newList.appendChild(span);
+}
+const addButton = document.getElementById("add");
+addButton.addEventListener("click", addTodo);
